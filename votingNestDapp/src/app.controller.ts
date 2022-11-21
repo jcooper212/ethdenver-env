7e49fecf01cd5503fb3f2f5dd4cfd08a7516f7b6
Jamshed Cooper <jcooper@jamsheds-imac.mynetworksettings.com>
2022-11-20 20:18:39 -0500

    voting dapp progress

--- votingNestDapp/src/app.controller.ts
+++ votingNestDapp/src/app.controller.ts
@@ -7,11 +7,6 @@ import {ethers} from 'ethers';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Get("last-block")
   getlastBlock() {
     return this.appService.getLastBlock();
@@ -22,31 +17,68 @@ export class AppController {
     return this.appService.getLastBlock(number);
   }
   
-  @Get("total-supply/:address")
-    getTotalSupply(@Param('address') address: string): Promise<number> {
+  @Get("total-supply")
+    getTotalSupply(@Query('address') address: string): Promise<number> {
     return this.appService.getTotalSupply(address);
   }
 
-  @Get("allowance")
-  getAllowance(@Query('address') address: string,
-    @Query('from') from: string,
-    @Query('to')  to: string): Promise<number> {
-    return this.appService.getAllowance(address, from, to);
-  }
+  // @Get("allowance")
+  // getAllowance(@Query('address') address: string,
+  //   @Query('from') from: string,
+  //   @Query('to')  to: string): Promise<number> {
+  //   return this.appService.getAllowance(address, from, to);
+  // }
 
 
-  @Get("payment-order/:id")
-  getPaymentOrder(@Param('id') id: number): any {
-    return this.appService.getPaymentOrder(id);
+  // @Get("payment-order/:id")
+  // getPaymentOrder(@Param('id') id: number): any {
+  //   return this.appService.getPaymentOrder(id);
+  // }
+
+  // @Post('payment-order')
+  // createPaymentOrder(@Body() body: PaymentOrderDTO) : number {
+  //   return this.appService.createPaymentOrder(body.value, body.secret);
+  // }
+
+  // @Post('request-payment')
+  // requestPaymentOrder(@Body() body: RequestPaymentOrderDTO) : Promise<any> {
+  //   return this.appService.requestPaymentOrder(body.value, body.secret, body.receiver);
+  // }
+
+////Voting DAPP functions
+  @Get("voting-token-address")
+  getTokenAddress() : any {
+    return this.appService.getTokenAddress();
   }
 
-  @Post('payment-order')
-  createPaymentOrder(@Body() body: PaymentOrderDTO) : number {
-    return this.appService.createPaymentOrder(body.value, body.secret);
+  @Get("voting-account")
+  getVotingAccount() : any {
+    return this.appService.getVotingAccount();
+  }
+  @Get("voting-balance")
+  getVotingAccountBalance() : any {
+    return this.appService.getVotingAccountBalance();
+  }
+  @Get("voting-power")
+  getVotingPower() : any {
+    return this.appService.getVotingPower();
+  }
+  @Get("voting-mint")
+  giveRightToVote() : any {
+    return this.appService.giveRightToVote();
+  }
+  @Get("voting-delegate")
+  delegateVote(@Param('address') address: string) : any {
+    console.log(`delg votes ${address}`);
+    return this.appService.delegateVote(address);
+  }
+  @Get("voting-vote")
+  vote(@Param('proposal') proposal: string) : any {
+    return this.appService.vote(proposal);
+  }
+  @Get("voting-create-wallet")
+  createNewWallet() : any {
+    return this.appService.createNewWallet();
   }
 
-  @Post('request-payment')
-  requestPaymentOrder(@Body() body: RequestPaymentOrderDTO) : Promise<any> {
-    return this.appService.requestPaymentOrder(body.value, body.secret, body.receiver);
-  }
 }
--- votingNestDapp/src/app.service.ts
+++ votingNestDapp/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { BigNumber, ethers } from 'ethers';
 import * as tokenJson from '../assets/VoteToken.json';
+import * as ballotJson from '../assets/TokenizedBallot.json';
+
 
 const ERC20VOTES_TOKEN_ADDRESS = "0x395061A11f4dAD5feCAfB44da775FC6C5229CEDd";
+const BALLOT_TOKEN_ADDRESS = "0xb55dFf80EB5B2813061Be67da8C681cdC139EACc";
+
 
 export class PaymentOrderDTO {
   value: number;
@@ -23,30 +27,41 @@ export class AppService {
   provider: ethers.providers.BaseProvider;
   erc20ContractFactory;
   wallet: ethers.Wallet | undefined;
+  signer: ethers.Wallet | undefined;
+  voteSigner: ethers.Wallet | undefined;
   etherBalance: number | undefined;
   tokenBalance: number | undefined;
   votesBalance: number | undefined;
   tokenContract: ethers.Contract | undefined;
+  ballotContract: ethers.Contract | undefined;
+  votingAccount: string | undefined;
+  mint_value: BigNumber | undefined;
+  mint_unit: BigNumber | undefined;
 
   paymentOrders: PaymentOrderDTO[];
 
   constructor(){
-    this.provider = ethers.getDefaultProvider('goerli');
-    this.erc20ContractFactory = new ethers.ContractFactory(
-      tokenJson.abi,
-      tokenJson.bytecode);
-    this.wallet = ethers.Wallet.createRandom().connect(this.provider);
-    this.tokenContract = new ethers.Contract(ERC20VOTES_TOKEN_ADDRESS, tokenJson.abi, this.wallet);
+    const provider = ethers.getDefaultProvider("goerli", {
+      alchemy: "g1CS1wIDRIhZb0_9mofYmODfLJmh8vlH",
+    });
+    this.wallet = new ethers.Wallet(process.env.ETH_P3);
+    this.signer = this.wallet.connect(provider);
+    this.voteSigner = this.signer;
+    this.votingAccount = this.signer.address;
+    this.mint_value = ethers.utils.parseEther("1");
+    this.mint_unit = this.mint_value.div(100);
+
+  
+    
+    // this.erc20ContractFactory = new ethers.ContractFactory(
+    //   tokenJson.abi,
+    //   tokenJson.bytecode);
+    // this.wallet = ethers.Wallet.createRandom().connect(this.provider);
+    this.tokenContract = new ethers.Contract(ERC20VOTES_TOKEN_ADDRESS, tokenJson.abi, this.signer);
+    this.ballotContract = new ethers.Contract(BALLOT_TOKEN_ADDRESS, ballotJson.abi, this.signer);
+
     //const signer = ethers.Wallet.fromMnemonic(process.env.ETH_M);
     this.paymentOrders = [];
-  }
-
-  getHello(): string {
-    return 'Hello World!';
-  }
-  
-  getElse(): string {
-    return 'Hello Else!';
   }
   
   getLastBlock(blockNum: string = 'latest') {
@@ -98,7 +113,7 @@ export class AppService {
     return tx.wait();
   }
 
-  createWallet(){
+  createAWallet(){
     this.wallet.getBalance().then((bal) => {
       this.etherBalance = parseFloat(ethers.utils.formatEther(bal));
     });
@@ -116,13 +131,74 @@ export class AppService {
         });
   }
 
-  vote(voteId: string){
-    const newPaymentOrder = new PaymentOrderDTO();
+  async giveRightToVote(){
+    const mintTx = await this.tokenContract.mint(this.votingAccount, this.mint_value);
+    await mintTx.wait();
+    console.log(`Minted ${this.mint_value.toString()} units to account ${this.votingAccount}`);
+    const bal = await this.tokenContract.balanceOf(this.votingAccount);
+    var votes = await this.tokenContract.getVotes(this.votingAccount);
+    console.log(`The balance is ${(await bal).toString()} for account ${this.votingAccount}
+      with voting power ${votes.toString()}`);
+
+    //Self Delegate
+    var delegateTx = await this.tokenContract.connect(this.signer).delegate(this.votingAccount);
+    await delegateTx.wait();
+    votes = this.tokenContract.getVotes(this.votingAccount);
+    console.log(`After delegate got ${(await votes).toString()} voting power for account ${this.votingAccount}`);
+    return (await votes).toString();
   }
+
+  async delegateVote(to_account: string){
+    //const to_wallet = new ethers.Wallet(to_account);
+    console.log(`delegate to ${to_account}`);
+    const transferTx = await this.tokenContract.connect(this.signer).transfer(to_account, this.mint_unit);
+    (await transferTx).wait();
+    var votes = await this.tokenContract.getVotes(this.votingAccount);
+    console.log(`After transfer VOTING ACCT got ${(await votes).toString()} voting power for account ${this.votingAccount}`)
+    var delegateTx = await this.tokenContract.connect(this.signer).delegate(to_account);
+    await delegateTx.wait();
+    votes = await this.tokenContract.getVotes(this.votingAccount);
+    console.log(`After delegation VOTING ACCT got ${(await votes).toString()} voting power for account ${this.votingAccount}`)
+    votes = await this.tokenContract.getVotes(to_account);
+    console.log(`After transfer DELEGATE_ACCT got ${(await votes).toString()} voting power for account ${to_account}`)
+  }
+
   getTokenAddress(){
     return ERC20VOTES_TOKEN_ADDRESS;
   }
-  requestToken(){
+  
+  getVotingAccount(){
+    return this.votingAccount;
   }
+  
+  async getVotingAccountBalance(){
+    const bal = await this.tokenContract.balanceOf(this.votingAccount);
+    return (await bal).toString();
+  }
+  
+  async getVotingPower(){
+    const bal = await this.tokenContract.getVotes(this.votingAccount);
+    return (await bal).toString();
+  }
+  
+  async createNewWallet(){
+    this.voteSigner = ethers.Wallet.createRandom().connect(this.provider);
+    // this.ballotContract = new ethers.Contract(BALLOT_TOKEN_ADDRESS, ballotJson.abi, this.voteSigner);
+
+    return this.voteSigner.address;
+  }
+  async vote(voteId: string){
+    var votes = await this.tokenContract.getVotes(this.voteSigner.address);
+    console.log(`Before Voting -- ${(await votes).toString()} voting power for account ${this.voteSigner.address}`);
+
+    const voteTx = await this.ballotContract.connect(this.voteSigner).vote(voteId, 5000);
+    (await voteTx).wait();
+
+    var votes = await this.tokenContract.getVotes(this.voteSigner.address);
+    console.log(`After Voting -- ${(await votes).toString()} voting power for account ${this.voteSigner.address}`);
+
+    return this.voteSigner.address;
+  }
+
 
 }
